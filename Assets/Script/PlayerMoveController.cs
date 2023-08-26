0c91d620d36f9dfdf07296b4ad5af64e040cb3b6
Vlad Kosik <barafrost666@mail.ru>
2023-08-27 02:25:58 +0700

    Add JoystickController

--- Assets/Script/PlayerMoveController.cs
+++ Assets/Script/PlayerMoveController.cs
@@ -7,6 +7,7 @@ public class PlayerMoveController : MonoBehaviour
     private Rigidbody rb;
     public float speed = 0.5f;
     private Vector3 moveVector;
+    public FixedJoystick joystick;
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
@@ -15,7 +16,12 @@ public class PlayerMoveController : MonoBehaviour
     
     void Update()
     {
-        moveVector.x = Input.GetAxis("Horizontal");
+        float moveX = joystick.Horizontal;
+        float moveY = joystick.Vertical;
+        moveVector = new Vector3(moveX, moveY, 0);
+        float rotZ = Mathf.Atan2(moveX, moveY) * Mathf.Rad2Deg;
+        var curentRotation = rb.rotation;
+        rb.rotation = Quaternion.Euler(-180, 0, rotZ);
         rb.velocity =  moveVector * speed;
     }
 }
